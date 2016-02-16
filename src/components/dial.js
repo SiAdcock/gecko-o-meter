5191232d57ef4dfd6929b31e88befdf040d7c01f
Simon Adcock <simonadcock2@gmail.com>
2016-02-16 08:31:20 +0000

    dial arm rotation example

--- src/components/dial.js
+++ src/components/dial.js
@@ -5,7 +5,7 @@ export default class Dial extends Component {
     return (
       <div className="dial">
         <div className="dial-image"/>
-        <div className="dial-arm"/>
+        <div className="dial-arm" style={{transform: 'rotate(90deg)', left: '80px', top: '50px'}}/>
       </div>
     );
   }
