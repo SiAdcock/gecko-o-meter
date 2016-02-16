cb89ca8403975144d498aec75303c9458f518f12
Simon Adcock <simonadcock2@gmail.com>
2016-02-16 08:10:44 +0000

    Component shuffling

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import data from './../data/data.json';
+import data from '../data/data.json';
 import { currencies } from '../constants';
 
 export default class App extends Component {
